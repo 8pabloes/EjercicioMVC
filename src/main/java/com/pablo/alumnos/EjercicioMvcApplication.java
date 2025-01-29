9d2bace40ac5090529a18ec2fe03162608e11ee7
8pabloes <8pabloes@gmail.com>
2025-01-29 12:39:50 +0100

    commit inicial

---
+++ src/main/java/com/pablo/alumnos/EjercicioMvcApplication.java
@@ -0,0 +1,13 @@
+package com.pablo.alumnos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EjercicioMvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EjercicioMvcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pablo/alumnos/EjercicioMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pablo.alumnos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EjercicioMvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
